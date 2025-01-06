440f7888762461cbb657ebf159a11a98edf1cad5
jaxmatrix <jay@live.co.in>
2025-01-06 09:05:55 +0530

    Adding doc string to the test layout slide

--- Templates/Test1.vb
+++ Templates/Test1.vb
@@ -1,10 +1,17 @@
 ﻿
 Namespace SlideTemplates
+    ''' <summary>
+    ''' Represents a test layout in the presentation. This slide will hold a title, description about the 
+    ''' title and a list of information. All this data will be used to descripbe a image that will be seen at 2:4 of
+    ''' of the width of the space
+    ''' </summary>
     Public Class Test1
+
         Private _title As String
         Private _description As Data.Content.Text
         Private _list As Data.Content.TextList
-        Private _image As Data
+        Private _imageSrc As String
+
 
         Public Sub New()
         End Sub
